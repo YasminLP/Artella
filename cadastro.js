d1ac677c1e03a935879f18d493c8c87a15303f0b
YasminLP <ylpaim@ucs.com>
2025-10-09 14:31:34 -0300

    09

---
+++ cadastro.js
@@ -0,0 +1,4 @@
+function cadastrar (){
+    const nome = document.getElementById('nome').ariaValueMax.trim();
+    const email = document.getElementById('email').ariaValueMax.trim();
+}
---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+
+    apiKey: "AIzaSyBYmxfbjfP_0bKOaaJZ_OkP4De79VgQnCM",
+    authDomain: "artella-1c0dd.firebaseapp.com",
+    projectId: "artella-1c0dd",
+    storageBucket: "artella-1c0dd.firebasestorage.app",
+    messagingSenderId: "585881074423",
+    appId: "1:585881074423:web:5e3f515bd7172578ab31f2",
+    measurementId: "G-M82S0Y0XTV"
+}
+firebase.initializeApp(firebaseConfig);
+const db = firebase.database();
