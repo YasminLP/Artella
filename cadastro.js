57feca473d802b40c25d363aa88a1c907f2dae7b
edertec <edertec@msn.com>
2025-10-16 13:48:48 -0300

    salvar usuario

--- cadastro.js
+++ cadastro.js
@@ -1,4 +1,25 @@
-function cadastrar (){
-    const nome = document.getElementById('nome').ariaValueMax.trim();
-    const email = document.getElementById('email').ariaValueMax.trim();
+function cadastrar(event) {
+    event.preventDefault();
+
+    const nome = document.getElementById('cadNome').value;
+    const email = document.getElementById('cadEmail').value;
+    const usuario = document.getElementById('cadUsuario').value;
+    const senha = document.getElementById('cadSenha').value;
+
+    const novoRegistro = {
+        nome,
+        email,
+        usuario,
+        senha
+    };
+
+    db.ref('usuarios').push(novoRegistro)
+        .then(() => {
+        alert("Registrado com sucesso!");
+        document.getElementById('cadNome').value = '';
+        document.getElementById('cadEmail').value = '';
+        document.getElementById('cadUsuario').value = '';
+        document.getElementById('cadSenha').value = '';
+
+    });
 }
